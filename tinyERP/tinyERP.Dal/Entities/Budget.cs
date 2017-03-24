799c5356a6d3b50cee1583660776af73d5c9392a
Luca Salzani <Alucard1766@users.noreply.github.com>
2017-03-24 11:11:05 +0100

    Added EF and Metro, Created first entity

---
+++ tinyERP/tinyERP.Dal/Entities/Budget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace tinyERP.Dal.Entities
+{
+    public class Budget
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public double Amount { get; set; }
+    }
+}
