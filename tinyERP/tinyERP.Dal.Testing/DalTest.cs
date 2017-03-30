7e89c8d940b6ca1d470e0070952ae912c54bfa02
Luca Salzani <Alucard1766@users.noreply.github.com>
2017-03-30 14:07:56 +0200

    Moved Tests and testenvironment to Folder Testing

---
+++ tinyERP/tinyERP.Dal.Testing/DalTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tinyERP.TestEnvrionment;
+
+namespace tinyERP.Dal.Testing
+{
+    [TestClass]
+    public class DalTest
+    {
+        [TestInitialize]
+        public void InitializeTestData()
+        {
+            TestEnvironmentHelper.InitializeTestData();
+        }
+
+        [TestMethod]
+        public void TestDBConnection()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
