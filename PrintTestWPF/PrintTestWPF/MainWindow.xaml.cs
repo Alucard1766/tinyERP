eedd875b802ced207e678c850c506f8a6620b4e9
Luca Salzani <Alucard1766@users.noreply.github.com>
2017-03-30 09:34:56 +0200

    Added Options to Printtest

--- PrintTestWPF/PrintTestWPF/MainWindow.xaml.cs
+++ PrintTestWPF/PrintTestWPF/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing.Printing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Spire.Pdf;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace PrintTestWPF
 {
@@ -47,11 +37,75 @@ namespace PrintTestWPF
             }
         }
 
-        private void btnPrintDocx_Click(object sender, RoutedEventArgs e)
+        private void btnOpenPdf_Click(object sender, RoutedEventArgs e)
         {
-            PrintDialog dialog = new PrintDialog();
-            this.pdfViewer1.PrintDialog = dialog;
+            Process.Start("Projektplan_tinyERPV1.0.pdf");
+        }
 
+        private void btnPrintPdfAdobe_Click(object sender, RoutedEventArgs e)
+        {
+            Pdf.PrintPDF(@"D:\Projekte\GitHub\tinyERP\PrintTestWPF\PrintTestWPF\bin\Debug\Projektplan_tinyERPV1.0.pdf");
+        }
+
+        private void btnOpenDocx_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("Projektplan_tinyERP.docx");
+        }
+
+        private void btnOpenUnknown_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("Projektplan_tinyERP.unknown");
         }
     }
+
+    public class Pdf
+    {
+        public static bool PrintPDF(string pdfFileName)
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.StartInfo.Verb = "print";
+
+                //Define location of adobe reader/command line
+                //switches to launch adobe in "print" mode
+                proc.StartInfo.FileName =
+                  @"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe";
+                proc.StartInfo.Arguments = String.Format(@"/p /h {0}", pdfFileName);
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+
+                proc.Start();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                if (proc.HasExited == false)
+                {
+                    proc.WaitForExit(10000);
+                }
+
+                proc.EnableRaisingEvents = true;
+
+                proc.Close();
+                KillAdobe("AcroRd32");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //For whatever reason, sometimes adobe likes to be a stage 5 clinger.
+        //So here we kill it with fire.
+        private static bool KillAdobe(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses().Where(
+                         clsProcess => clsProcess.ProcessName.StartsWith(name)))
+            {
+                clsProcess.Kill();
+                return true;
+            }
+            return false;
+        }
+    }//END Class
 }
