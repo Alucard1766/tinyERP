b39a0d8d6c743c33d161fc2d21e583656489b3f1
Fabian Gübeli <fabian.guebeli@hsr.ch>
2017-04-27 13:41:25 +0200

    Add Basic Customer View

---
+++ tinyERP/tinyERP/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using tinyERP.Dal.Entities;
+using tinyERP.UI.Factories;
+
+namespace tinyERP.UI.ViewModels
+{
+    internal class CustomerViewModel : ViewModelBase
+    {
+        public Customer SelectedCustomer { get; set; }
+
+        public CustomerViewModel(IUnitOfWorkFactory factory) : base(factory)
+        {
+        }
+
+        public ObservableCollection<Customer> CustomerList { get; set; }
+
+        public override void Load()
+        {
+            var customers = UnitOfWork.Customers.GetAll();
+            CustomerList = new ObservableCollection<Customer>(customers);
+        }
+
+        #region New-Customer-Command
+
+        private RelayCommand _newCustomerCommand;
+
+        public ICommand NewCustomerCommand
+        {
+            get { return _newCustomerCommand ?? (_newCustomerCommand = new RelayCommand(param => NewCustomer(), param => CanNewCustomer())); }
+        }
+
+        private void NewCustomer()
+        {
+            //var transaction = new Transaction()
+            //{
+            //    IsRevenue = true,
+            //    Date = DateTime.Today
+            //};
+            //var vm = new EditTransactionViewModel(new UnitOfWorkFactory(), transaction);
+            //vm.Init();
+            //var window = new EditTransactionView(vm);
+
+            //if (window.ShowDialog() ?? false)
+            //{
+            //    CustomerList.Add(customer);
+            //}
+        }
+
+        private bool CanNewCustomer()
+        {
+            return true;
+        }
+
+        #endregion
+
+    }
+}
