9ae9f7859a6f53735219db6dcc8519dec1bd88eb
Giovanni Heilmann <giovanni.heilmann@gmail.com>
2017-04-28 19:58:43 +0200

    Added Document to Dal

--- tinyERP/tinyERP.Dal/UnitOfWork.cs
+++ tinyERP/tinyERP.Dal/UnitOfWork.cs
@@ -8,6 +8,7 @@ namespace tinyERP.Dal
 
         private IBudgetRepository _budgets;
         private ICategoryRepository _categories;
+        private IDocumentRepository _documents;
         private ITransactionRepository _transactions;
 
         public UnitOfWork(TinyErpContext context)
@@ -18,6 +19,8 @@ namespace tinyERP.Dal
         public IBudgetRepository Budgets => _budgets ?? (_budgets = new BudgetRepository(context));
 
         public ICategoryRepository Categories => _categories ?? (_categories = new CategoryRepository(context));
+
+        public IDocumentRepository Documents => _documents ?? (_documents = new DocumentRepository(context));
 
         public ITransactionRepository Transactions => _transactions ?? (_transactions = new TransactionRepository(context));
 
